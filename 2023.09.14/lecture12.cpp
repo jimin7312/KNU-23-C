56000431164dff5b278739847e52114645e4019c
jimin7312 <jimin7312@naver.com>
2023-09-14 12:26:49 +0900

    OX

---
+++ 2023.09.14/lecture12.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	
+	for (a = 0; a < 5; a++) {
+		for (b = 0; b < a; b++) {
+			printf("    ");
+		}
+		if (a % 2 == 0) printf("| O |");
+		else printf("| X |");
+		printf("\n------------------------------\n");
+	}
+	return 0;
+}
