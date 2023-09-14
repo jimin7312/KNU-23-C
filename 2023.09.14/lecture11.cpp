3997eb5ec94e2e9768047b2b40ea338d9d6c663c
jimin7312 <jimin7312@naver.com>
2023-09-14 12:15:42 +0900

    while

---
+++ 2023.09.14/lecture11.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	i = 2;
+	j = 1;
+	while(i<=9){
+		if (i == 5) {
+			i++;
+			continue;
+		}
+		while (j <= 9) {
+			printf("%d*%d=%d\n", i, j, i * j);
+			j++
+		}
+		j++;
+		j = 1;
+	}
+	return 0;
+}
